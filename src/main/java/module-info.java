8d85374fdca8617d21f37599eced40fb9e273fe9
Isaacmart <65898932+Isaacmart@users.noreply.github.com>
2022-01-12 21:07:56 -0500

    setup automatic bulding features

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+/**
+* Provides the starter code for the <strong>cs1302-omega</strong> project.
+*/
+module office.portal {
+   requires transitive java.logging;
+   requires transitive javafx.controls;
+   requires transitive com.google.gson;
+} // module
+{
+    
+
---
+++ src/main/java/office/portal/Portal.java
@@ -0,0 +1,11 @@
+package office.portal;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Portal extends Application{
+    
+    public void start(Stage stage){
+        
+    }
+}
